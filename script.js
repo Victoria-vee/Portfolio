f544be77d9244dd09dd675e1c8870548cc0cff67
Victoria Essien <145772042+Victoria-vee@users.noreply.github.com>
2026-01-19 15:21:31 +0100

    Update contact form and email functionality; change carousel item color

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const links = document.querySelector("header .links");
 hamburger.addEventListener("click", () => {
   links.classList.toggle("active");
 });
+
+document
+  .getElementById("contactform")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Send message to YOU
+emailjs.send("service_32s7afk","template_7m7u8lb").then(() => {
+
+      // // Send auto-reply to USER
+      // emailjs.sendForm(
+      //   "YOUR_SERVICE_ID",
+      //   "portfolio_autoreply",
+      //   this
+      // );
+
+      alert("Message sent successfully");
+    }).catch(() => {
+      alert("Failed to send message");
+    });
+  });
